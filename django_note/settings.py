0d3bf41b77e8ff124eb9e88381816f7743bf1ab4
Jesse Chang <lcf.zhangxing@hotmail.com>
2020-11-15 01:58:58 +0800

    ✨feat(激活之前的创建的应用):

--- django_note/settings.py
+++ django_note/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app01.apps.App01Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
 ]
 
 MIDDLEWARE = [
